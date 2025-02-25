30221407d8e470146c0cb76ffba0c8a3d8066e95
gopal.lin <gopal.lin@evolutivelabs.com>
2025-02-25 15:53:09 +0800

    feat: add new function that can close all by group

--- lua/config/keybindings.lua
+++ lua/config/keybindings.lua
@@ -53,6 +53,7 @@ end
 
 map("n", "<leader>9", bufferline.jump_to_last_buffer)
 map("n", "<leader>bb", bufferline.switch_to_previous_buffer)
+map("n", "<leader>bg", bufferline.close_others_in_group)
 map("n", "<leader>ba", ":BufferLineCloseOthers<CR>")
 map("n", "<leader>be", ":bnext<CR>")
 map("n", "<leader>bd", ":bnext<CR>:bd#<CR>")
--- lua/utility/bufferline.lua
+++ lua/utility/bufferline.lua
@@ -14,7 +14,30 @@ local function switch_to_previous_buffer()
   end
 end
 
+local function close_others_in_group()
+    local bufferline_api = require("bufferline")
+    local current_buf = vim.api.nvim_get_current_buf()
+    local current_buftype = vim.api.nvim_buf_get_option(current_buf, "buftype")
+    local is_terminal = current_buftype == "terminal"
+
+    -- Get all buffers from bufferline
+    local buffers = bufferline_api.get_elements().elements
+
+    for _, buf in ipairs(buffers) do
+        local buf_id = buf.id
+        if buf_id ~= current_buf then -- Skip the current buffer
+            local buf_buftype = vim.api.nvim_buf_get_option(buf_id, "buftype")
+            local in_same_group = (is_terminal and buf_buftype == "terminal") or
+                                 (not is_terminal and buf_buftype ~= "terminal")
+            if in_same_group then
+                vim.api.nvim_buf_delete(buf_id, { force = true })
+            end
+        end
+    end
+end
+
 M.jump_to_last_buffer = jump_to_last_buffer
 M.switch_to_previous_buffer = switch_to_previous_buffer
+M.close_others_in_group = close_others_in_group
 
 return M
