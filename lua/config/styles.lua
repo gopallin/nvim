7401ae0e805d369ce71499ce7a94a8c467e14a2c
gopal.lin <gopal.lin@evolutivelabs.com>
2025-09-12 17:05:33 +0800

    feat: add color theme

--- lua/config/styles.lua
+++ lua/config/styles.lua
@@ -1,5 +1,5 @@
-vim.cmd("colorscheme vim")
---vim.cmd("colorscheme habamax")
+-- vim.cmd("colorscheme vim")
+-- vim.cmd("colorscheme lackluster")
 
 --vim.cmd("highlight CursorLine ctermbg=black guibg=black")
 --vim.cmd("highlight CursorColumn ctermbg=black guibg=black")
@@ -7,3 +7,4 @@ vim.cmd("colorscheme vim")
 vim.api.nvim_set_hl(0, "javaScriptParens", { link = "NonText" })
 vim.api.nvim_set_hl(0, "phpRegion", { link = "DevIconElisp" })
 vim.api.nvim_set_hl(0, "Comment", { link = "DevIconAsc" })
+
--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -59,5 +59,18 @@ require("lazy").setup({
   { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" }, event = "VimEnter" },
 
   { "lukas-reineke/indent-blankline.nvim" },
+
+  {
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('github-theme').setup({
+      })
+
+      vim.cmd('colorscheme github_dark')
+    end,
+  }
 })
 
